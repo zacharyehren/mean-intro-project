308ab39250bd82fbccbca6ab0122679a385c4ed3
Zachary Ehren <zachehren@gmail.com>
2018-10-08 11:56:29 -0700

    set up routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MatToolbarModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ListComponent } from './components/list/list.component';
 import { CreateComponent } from './components/create/create.component';
 import { EditComponent } from './components/edit/edit.component';
+
+const routes: Routes = [
+  { path: 'create', component: CreateComponent},
+  { path: 'edit/:id', component: EditComponent },
+  { path: 'list', component: ListComponent},
+  // pathMatch will only redirect with an exact match
+  { path: '', redirectTo: 'list', pathMatch: 'full'}
+];
 
 @NgModule({
   declarations: [
@@ -16,7 +27,9 @@ import { EditComponent } from './components/edit/edit.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(routes),
+    MatToolbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
