490b912b0621e251ae963743a95366d99c525cbf
Zachary Ehren <zachehren@gmail.com>
2018-10-08 17:02:32 -0700

    list component complete

--- src/app/components/list/list.component.ts
+++ src/app/components/list/list.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MatTableDataSource } from '@angular/material';
+
+import { Issue } from '../../issue.model';
 import { IssueService } from '../../issue.service';
 
 @Component({
@@ -7,13 +11,35 @@ import { IssueService } from '../../issue.service';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+  
+  issues: Issue[];
+  // to be used with the MatTable
+  displayedColumns = ['title', 'responsible', 'severity', 'status', 'actions'];
 
-  constructor(private issueService: IssueService) { }
+  constructor(private issueService: IssueService, private router: Router) { }
 
   ngOnInit() {
-    this.issueService.getIssues().subscribe((issues) => {
-      console.log(issues);
-    })
+    this.fetchIssues();
+  }
+
+  fetchIssues() {
+    this.issueService
+      .getIssues()
+      .subscribe((data: Issue[]) => {
+        this.issues = data;
+        console.log('Data requested...');
+        console.log(this.issues);
+      });
+  }
+
+  editIssue(id) {
+    this.router.navigate([`/edit/${id}`]);
+  }
+
+  deleteIssue(id) {
+    this.issueService.deleteIssue(id).subscribe(() => {
+      this.fetchIssues();
+    });
   }
 
 }
